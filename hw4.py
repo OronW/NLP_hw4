dde396f37b8a0b2c25a6ccd292508c32864bbad4
oronW <oronwerner@gmail.com>
2020-05-29 13:15:55 +0300

    created a list of lists for all sentences

--- hw4.py
+++ hw4.py
@@ -1,12 +1,28 @@
 import os
 
 path = r'C:\Users\oron.werner\PycharmProjects\NLP\hw4Inputs\allCountryFiles'
-
+inputAllCountries = r'C:\Users\oron.werner\PycharmProjects\NLP\hw4Inputs\allCountryFiles\SumOfAll.txt'
 
 def main():
 
-    combineFilesIntoOne(path)
+    sentencesList = sentencesToListOfLists(inputAllCountries)
+    print(len(sentencesList))
 
+
+
+def sentencesToListOfLists(inputPath):
+    sentencesList = []
+    lineList = []
+
+    f = open(inputPath, 'r', encoding='utf-8')
+    for line in f:
+        splittedLine = line.rstrip().split()
+        for word in splittedLine:
+            lineList.append(word)
+        sentencesList.append(lineList)
+        lineList = []
+
+    return sentencesList
 
 # This function was used once to combine all country files into one
 def combineFilesIntoOne(directory):
