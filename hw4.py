561a6b8d6e7dc2035cc76d0951a6afc6320f5afd
oronW <oronwerner@gmail.com>
2020-05-31 01:18:14 +0300

    importing the kfolds and features vectors of hw3. Testing method for word searching and results of vector of words

--- hw4.py
+++ hw4.py
@@ -4,6 +4,13 @@ import gensim
 from gensim.models import Word2Vec
 import gc
 from gensim.models import KeyedVectors
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.utils._testing import ignore_warnings
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+from sklearn import metrics
 
 
 path = r'C:\Users\oron.werner\PycharmProjects\NLP\hw4Inputs\allCountryFiles'
@@ -12,14 +19,94 @@ hw4Inputs = r'C:\Users\oron.werner\PycharmProjects\NLP\hw4Inputs'
 pathSelfTrained = r'C:\Users\oron.werner\PycharmProjects\NLP\hw4Inputs\self_trained_model.vec'
 pathPreTrained = r'C:\Users\oron.werner\PycharmProjects\NLP\hw4Inputs\wiki.en.100k.vec'
 allUsersOfCountry = r'C:\Users\oron.werner\PycharmProjects\NLP\hw4Inputs\allUsersOfCountryPhaseB'
+chunkPath = r'C:\Users\oron.werner\PycharmProjects\NLP\hw4Inputs\allUsersOfCountryPhaseB\chunk'
 
 
 def main():
 
-    combineSentences(allUsersOfCountry)
+    # combineSentences(allUsersOfCountry)
+    # totalCorpus = readAndLabel(chunkPath)
+    # createFeatureVectors(totalCorpus, 'LR', vectorType='manual')
+    model = KeyedVectors.load_word2vec_format(pathPreTrained, binary=False)
+    if 'and' in model.vocab:
+        print('HURRAY!')
+
+    if 'etgf' in model.vocab:
+        print('haa!')
+    # print(model.word_vec('and')[:2])
+    # print(model.get_vector('and')[:2])
+
+
+@ignore_warnings(category=ConvergenceWarning)
+def createFeatureVectors(totalCorpus, classifier, featureList=None, vectorType='normal'):
+
+    totalDf = pd.DataFrame.from_dict(totalCorpus)     # create a data frame for the labeled sentences
+    y = totalDf['class']     # create a column for of the labels
+    X = totalDf['text']
+
+    sum = 0
+
+    kf = KFold(n_splits=10, random_state=1, shuffle=True)
+
+    for train_index, test_index in kf.split(X):
+        # print("\nTRAIN:", train_index, "TEST:", test_index)
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+
+        if vectorType == 'manual':
+            myVectorXtrain = []
+            myVectorXtest = []
+
+            for sentence in X_train:
+                st = sentence.split()
+                myVectorXtrain.append([len(sentence), len(st), sentence.count('!'), sentence.count('?'), sentence.count('.'), sentence.count('\''), sentence.count('I am'), sentence.count('you \' re'), sentence.count('. . .'), sentence.count('I \' m')])
+
+            for sentence in X_test:
+                st = sentence.split()
+                myVectorXtest.append([len(sentence), len(st), sentence.count('!'), sentence.count('?'), sentence.count('.'), sentence.count('\''), sentence.count('I am'), sentence.count('you \' re'), sentence.count('. . .'), sentence.count('I \' m')])
+
+
+            X_train_dtm = np.array(myVectorXtrain)  # create document - term matrix for the words
+            X_test_dtm = np.array(myVectorXtest)
 
 
 
+        # -- this part for LR classifier --
+        elif classifier == 'LR':
+            lr = LogisticRegression(max_iter=500)
+            lr.fit(X_train_dtm, y_train)
+            y_pred_class = lr.predict(X_test_dtm)
+            sum += metrics.accuracy_score(y_test, y_pred_class)
+
+        else:
+            print('NO CLASSIFIER SELECTED FOR \'createFeatureVectors\' FUNCTION. ENDING RUN! ')
+            exit()
+
+
+        # print('\nAccuracy score: ', metrics.accuracy_score(y_test, y_pred_class))
+        # print('Test sentences by classes:')
+        # print(y_test.value_counts())
+        # print('Confusion matrix:\n', metrics.confusion_matrix(y_test, y_pred_class))
+
+    # print('\n**********************************')
+
+    acc = sum/10
+    total = int(sum*1000)/100
+
+    if classifier == 'NB':
+        print('Naïve Bayes:', total)
+        summary = str('Naïve Bayes: ' + str(total))
+        # summaryToFile.append(summary)
+
+    if classifier == 'LR':
+        print('Logistic Regression: ', total)
+        summary = str('Logistic Regression: ' + str(total))
+        # summaryToFile.append(summary)
+
+
+
+# TODO: make part of combinedSentences?
 def readAndLabel(directory):
 
     label = 0
@@ -59,7 +146,7 @@ def combineSentences(folderPath):
             f = open(path1, 'r', encoding='utf-8')
             sentences = f.read().splitlines()
 
-            newPath = folderPath + '\\chunck\\' + 'combined20' + currentFile
+            newPath = folderPath + '\\chunk\\' + 'combined20' + currentFile
 
             f = open(newPath, 'w', encoding='utf-8')
             counter = 0
